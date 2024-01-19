b9c1bd155e8254de676c34c289de7e671cf713a7
Xylobyte <nantsamontillet@xylobyte.com>
2024-01-19 21:58:20 +0100

    Add base code for home

---
+++ scripts/api/phtographer.js
@@ -0,0 +1,13 @@
+async function API_Photographers_enum() {
+    const response = await fetch('/data/photographers.json');
+    const result = await response.json();
+
+    return result.photographers;
+}
+
+async function API_Photographer_get(id) {
+    const response = await fetch(`/data/photographers.json`);
+    const result = await response.json();
+
+    return result.find(p => p.id === id);
+}
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,18 +1,42 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+class Photographer {
+    constructor(data) {
+        this._name = data.name;
+        this._id = data.id;
+        this._city = data.city;
+        this._country = data.country;
+        this._tagline = data.tagline;
+        this._price = data.price;
+        this._portrait = data.portrait;
     }
 
-    return { name, picture, getUserCardDOM }
+    getUserCardDOM() {
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute("src", `assets/photographers/${this._portrait}`);
+        const h2 = document.createElement('h2');
+        h2.textContent = this._name;
+        article.appendChild(img);
+        article.appendChild(h2);
+
+        const infoDiv = document.createElement('div');
+        const location = document.createElement('span');
+        location.textContent = `${this._city}, ${this._country}`;
+        const tagline = document.createElement('p');
+        tagline.textContent = this._tagline;
+        const price = document.createElement('span');
+        price.textContent = `${this._price}€/jour`;
+
+        infoDiv.appendChild(location);
+        infoDiv.appendChild(tagline);
+        infoDiv.appendChild(price);
+
+        article.appendChild(infoDiv);
+
+        const link = document.createElement('a');
+        link.setAttribute("href", `photographer.html?id=${this._id}`);
+
+        link.appendChild(article);
+
+        return (link);
+    }
 }
